e85d90fa12dbfe6986d51cf786cf9287f4e920c9
hollymellodic <hollymellodic@gmail.com>
2018-11-20 07:00:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php session_start(); /* Starts the session */
+if(!isset($_SESSION['UserData']['Username'])){
+header("location:login.php");
+exit;
+}
+?>
+
+<iframe src="index.html" style="width:100%; height:700px; border:0;"></iframe>
+<br>
+<center>Click <a href="logout.php">here</a> to Logout.</center>
